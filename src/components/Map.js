cf746f3fa820f3351e8eef831dde3c4a7ea823d0
Wenny Hidayat <wenny.h@live.com>
2017-07-29 16:27:44 +1000

    Added location data and pin mapping

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export default class HereMap extends Component {
 	componentDidMount() {
+		const careProvider = require('../data/careProvider.json');
 		this.initializeCredential();
 
 		// Obtain the default map types from the platform object:
@@ -15,17 +16,26 @@ export default class HereMap extends Component {
 				zoom: 14,
 				center: { lat: -33.8688, lng: 151.2093 }
 			});
-
 		const behavior = new window.H.mapevents.Behavior(new window.H.mapevents.MapEvents(map));
 		const ui = window.H.ui.UI.createDefault(map, defaultLayers);
+
+		const geocoder = window.platform.getGeocodingService();
+		careProvider.map( (place) => {
+			place.LATITUDE && place.LONGITUDE &&
+			this.addMarkersToMap({mapTarget: map, lat: place.LATITUDE, lng: place.LONGITUDE}) 
+		});
 	}
 
 	initializeCredential = () => {
-		console.log('Initializing here');
 		window.platform = new window.H.service.Platform({
 			'app_id': 'hB3opaBMaVphS4nFxb5W',
 			'app_code': 'pCNQnASsLB9YnsLFd15eUw'
 		});
+	}
+
+	addMarkersToMap({mapTarget, lat, lng}) {
+		const marker = new window.H.map.Marker({lat:lat, lng:lng});
+		mapTarget.addObject(marker);
 	}
 
 	render() {
