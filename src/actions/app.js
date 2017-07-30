726a4e5cf6c4fefc104e13c49c5278e52fddbf09
Wenny Hidayat <wenny.h@live.com>
2017-07-30 12:24:03 +1000

    Added more actions

--- src/actions/app.js
+++ src/actions/app.js
@@ -6,3 +6,24 @@ export function updateSuburb(suburb) {
     suburb: suburb
   }
 }
+
+export function updateCapacity(capacity) {
+  return {
+    type: actionTypes.UPDATE_CAPACITY,
+    capacity: capacity
+  }
+}
+
+export function updateBudget(budget) {
+  return {
+    type: actionTypes.UPDATE_BUDGET,
+    budget: budget
+  }
+}
+
+export function updateLocation(location) {
+  return {
+    type: actionTypes.UPDATE_LOCATION,
+    location: location
+  }
+}
