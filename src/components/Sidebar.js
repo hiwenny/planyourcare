ecef07d352f52544ebc609afb7d1d250c665abec
Wenny Hidayat <wenny.h@live.com>
2017-07-30 14:36:24 +1000

    budget fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -30,7 +30,7 @@ const locationsList = createOptionsFromJSON('SA3_name');
 const locationsFilter = createFilterOptions({ options: locationsList });
 
 const budgetsList = [
-    {value: 150, label: 150},
+    {value: 50, label: 50},
     {value: 100, label: 100},
     {value: 150, label: 150},
     {value: 200, label: 200},
